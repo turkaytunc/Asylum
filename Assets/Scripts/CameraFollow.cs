c25c21b12bcaa74d28f8cf7547de387a3de63f99
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-10 23:40:30 +0300

    added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform playerTransform;
+
+
+
+    private void Update()
+    {
+        transform.position = playerTransform.position;
+    }
+}
