a2e7f5d0ac03c9654ffdfa707710c4ffdb0d061f
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-18 22:35:17 +0300

    Added: click to move

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    ThirdPersonCharacter character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.transform.parent.GetComponent<CameraRaycaster>();
+        character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit" + cameraRaycaster.Hit.collider.name);
+            currentClickTarget = cameraRaycaster.Hit.point;  // So not set in default case
+        }
+        character.Move(currentClickTarget - transform.position, false, false);
+    }
+}
+
